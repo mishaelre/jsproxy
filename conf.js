32c87ff79eb4684ae8399eba3a61409bf2c90639
mishaelre <664513537@qq.com>
2020-03-28 18:11:37 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -88,8 +88,12 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'http://haha.com/': {
+    'http://example.com/': {
       content: 'Hello World'
     },
+    'https://www.pornhub.com/': {
+      redir: 'https://php.net/'
+    },
+    
   }
 })
